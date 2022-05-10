e961a64d47d1cff3982324bfba1acf6700832fa2
Sporplut <77099788+Sporplut@users.noreply.github.com>
2022-05-10 20:56:49 +0800

    Create userid.lua

---
+++ userid.lua
@@ -0,0 +1,16 @@
+userids = {
+[137640845] = true, --ejmd
+[69853906] = true, --ditch
+[3122289773] = true, --johny
+[3448550566] = true, --gwapo7
+[2383758777] = true, --grid
+[2383748814] = true, --grid
+[1863285988] = true, --justine
+[1707978654] = true, --justine
+[3415212026] = true, --justine
+[2864537584] = true, --justine
+[3360429523] = true, --donnell
+[3361872585] = true, --donnell
+[3119239833] = true --exequiel
+}
+return userids
